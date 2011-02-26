c250e0fa22b5d80e5bd9a759d8ac9e14dedcb8cf
valbertovc <valbertovc@gmail.com>
2011-02-26 15:04:36 -0300

    teste

--- exercicio2.c
+++ exercicio2.c
@@ -1,49 +1,55 @@
 /* Data: 24/02/2011
  * Programador: Valberto Carneiro
- * Objetivo: Faça um programa que leia uma string do teclado e diga se ela é 
-             palíndrome. Uma string é palíndrome quando pode ser lida tanto 
-             de trás pra frente quanto de frente para trás e possui exatamente 
-             a mesma seqüência de caracteres. Ex.: ASA, SUBI NO ONIBUS. 
-             Desconsidere os espaços. 
-             Defina uma função chamada Palindrome que receba uma string como 
-             parâmetro e retorne um boolean no seu programa. 
-             Dica: Use a função do exercício 1.
+ * Objetivo: Faca um programa que leia uma string do teclado e diga se ela e 
+             palindrome. Uma string e palíndrome quando pode ser lida tanto 
+             de tras pra frente quanto de frente para tras e possui exatamente 
+             a mesma sequencia de caracteres. Ex.: ASA, SUBI NO ONIBUS. 
+             Desconsidere os espacos. 
+             Defina uma funcao chamada Palindrome que receba uma string como 
+             parametro e retorne um boolean no seu programa. 
+             Dica: Use a funcao do exercicio 1.
  */
 
 #include<stdio.h>
 const int FALSE = 0;
 const int TRUE = -1;
 
-int is_palindrome(char *texto) {
-    int palindrome = TRUE, tamanho = strlen(texto), y = 0, x = 0;
-    
-    //elimina brancos
-    while(x < tamanho) {
-        if(texto[x] == ' ') for(y=x; y < tamanho; y++) texto[y] = texto[y+1];
-        x++;
-    }
-    
-    tamanho = strlen(texto);
-    
-    //compara os caracteres
-    for(x = 0; x < tamanho ; x++) {
-       //printf("%c = %c\n", texto[x], texto[tamanho-1-x]);
-       if (texto[x] != texto[tamanho-1-x]) {
-          palindrome = FALSE;
-          break;
-       }
-    }
-    return palindrome;
-}
+//protoipos dos metodos
+void retira_brancos(char *texto);
+int is_palindrome(char *texto);
 
 int main(){
-    char texto[100];
-    printf("Escreva uma string para sabermos se ela é palindrome: ");
-    gets(texto);
+  char texto[100];
+  printf("Escreva uma string para sabermos se ela e palindrome: ");
+  gets(texto);
     
-    //printf("Resultado: %s", texto);
-    printf("Resultado: %s", is_palindrome(texto) == FALSE ? "nao" : "sim");
-    getch();
+  //printf("Resultado: %s", texto);
+  printf("Resultado: %s", is_palindrome(texto) == FALSE ? "nao" : "sim");
+  getch();
 }
 
+void retira_brancos(char *texto) {
+  int y = 0, x = 0, tamanho = strlen(texto);
+  
+  while(x < tamanho) {
+    if(texto[x] == ' ') for(y=x; y < tamanho; y++) texto[y] = texto[y+1];
+    x++;
+  }    
+}
 
+int is_palindrome(char *texto) {
+  int palindrome = TRUE, tamanho = strlen(texto), x;
+  
+  retira_brancos(texto);
+  tamanho = strlen(texto);
+  
+  //compara os caracteres
+  for(x = 0; x < tamanho ; x++) {
+    //printf("%c = %c\n", texto[x], texto[tamanho-1-x]);
+    if (texto[x] != texto[tamanho-1-x]) {
+       palindrome = FALSE;
+       break;
+    }
+  }
+  return palindrome;
+}
